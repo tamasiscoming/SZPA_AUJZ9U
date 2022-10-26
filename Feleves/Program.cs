91544563a6f3c6c8e7f5d5d9d36759c165297928
tamasiscoming <nvntamas@gmail.com>
2022-10-26 14:59:11 +0200

    re2 into console app

---
+++ Feleves/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Feleves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap originalImage = null;
+            int imageWidth = 0;
+            int imageHeight = 0;
+
+            Console.WriteLine("Press -B- to Browse an image");
+            Console.WriteLine("Press -Q- to Quit the program");
+
+            if (Console.ReadLine().ToUpper() == "B")
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = new Bitmap(open.FileName);
+                    imageWidth = originalImage.Width;
+                    imageHeight = originalImage.Height;
+                }
+            }
+            else if (Console.ReadLine().ToUpper() == "Q")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
