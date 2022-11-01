dbab64b210fc94ebc5e630123836aa1df58fc5ff
tamasiscoming <nvntamas@gmail.com>
2022-11-01 13:14:03 +0100

    Szekvenciális működik tökéletesen. -> todo lock (lockolo) {...} Error: az objektum már használva van

--- SZPA/SZPA/Program.cs
+++ SZPA/SZPA/Program.cs
@@ -9,38 +9,109 @@ using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
 using System.Globalization;
+using static System.Windows.Forms.LinkLabel;
+using System.Collections;
+using System.Data.Common;
 
 namespace SZPA
 {
     internal class Program
     {
-        static Bitmap newImage = null;
+        static Bitmap newImage = FileBrowse();
         static string asciiChars = " .:-=+*#%@";
         static Stopwatch sw = new Stopwatch();
+        static string path = "test.txt";
+        static List<string> lines;
+        static int dividedBy = newImage.Width / 100;
 
         [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Please browse an image");
-            Thread.Sleep(1000);
-
-            newImage = FileBrowse();
-            SeqMakeArt(newImage);
+            //SeqMakeArt(newImage);
+            ParMakeArt(newImage);
 
             Console.WriteLine("Finished MakeArt()");
             Thread.Sleep(1000);
+            Console.ReadLine();
             Environment.Exit(0);
         }
+
+        #region Parallel Approach
+        #region ???
+        static void ParMakeArt(Bitmap newImage)
+        {
+            sw.Start();
+            // most kell valahova lockolni lock (lockolo) {...}
+            Parallel.For(0, newImage.Height, row => {
+                for (int column = 0; column < newImage.Width; column++)
+                {
+                    var pixel = newImage.GetPixel(column, row);
+                    var avg = (pixel.R + pixel.G + pixel.B) / 3;
+                    var c = asciiChars[avg * asciiChars.Length / 255 % asciiChars.Length];
+
+                    Console.SetCursorPosition(column, row);
+                    Console.Write(c);
+                }
+
+                //lines.Append(c.ToString());
+            });
+
+            //var lockolo = new Object();
+            //Parallel.For(0, newImage.Size.Width, e =>
+            //{
+            //    lock (lockolo)
+            //    {
+            //        var pixel = newImage.GetPixel(newImage.Width, newImage.Height);
+            //        var avg = (pixel.R + pixel.G + pixel.B) / 3;
+            //        var c = asciiChars[avg * asciiChars.Length / 255 % asciiChars.Length];
+
+            //        Console.SetCursorPosition(newImage.Width, newImage.Height);
+            //        Console.Write(c);
+            //        lines.Append(c.ToString());
+            //    }
+
+            //    lines.Append("\n");
+            //});
+
+            //Thread[] threads = new Thread[newImage.Size.Height];
+
+            //for (int row = 0; row < newImage.Height; row++)
+            //{
+            //    threads[row] = new Thread(() => ThreadJob(newImage, row));
+            //    threads[row].Start();
+            //}
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+        #endregion
+
+        static void ThreadJob(Bitmap newImage, int row)
+        {
+            lines = new List<string>();
+            for (int column = 0; column < newImage.Width; column++)
+            {
+                var pixel = newImage.GetPixel(column, row);
+                var avg = (pixel.R + pixel.G + pixel.B) / 3;
+                var c = asciiChars[avg * asciiChars.Length / 255 % asciiChars.Length];
+
+                Console.SetCursorPosition(row, column);
+                Console.Write(c);
+                lines.Append(c.ToString());
+            }
+
+            lines.Add("\n");
+            File.WriteAllLines(path, lines);
+        }
+        #endregion
+
         #region Sequentional Approach
         static void SeqMakeArt(Bitmap newImage)
         {
             sw.Start();
-            var dividedBy = newImage.Width / 100;
-            var height = newImage.Height;
 
             newImage = new Bitmap(newImage, new Size(newImage.Width / dividedBy, newImage.Height / dividedBy));
-            List<string> lines = new List<string>();
-            var path = "test.txt";
+            lines = new List<string>();
 
             for (int i = 0; i < newImage.Height; i++)
             {
@@ -53,10 +124,9 @@ namespace SZPA
                     var c = asciiChars[avg * asciiChars.Length / 255 % asciiChars.Length];
                     Console.Write(c);
                     line += c.ToString();
+                    lines.Append(c.ToString());
                 }
                 lines.Add(line + "\n");
-
-                Console.WriteLine();
             }
 
             Console.WriteLine("-----------------------------------------------------------------------------------------------------------" +
